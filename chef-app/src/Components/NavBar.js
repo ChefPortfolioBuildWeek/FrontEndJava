1ae54f0cd6bcd19ee959df5fa961f850fd84cd76
vpagano10 <vpagano10@gmail.com>
2019-10-23 12:41:51 -0400

    navbar changes

--- chef-app/src/Components/NavBar.js
+++ chef-app/src/Components/NavBar.js
@@ -13,33 +13,23 @@ const NavDiv = styled.div`
   display: flex;
   justify-content: space-around;
   margin: 0 auto;
-`;
-const NavLinks = styled.p`
-  text-decoration: none;
-  color: black;
-  display: flex;
-  justify-content: space-around;
-  margin: 0;
-  display: flex;
-  justify-content: space-around;
-  margin: 0 auto;
   background-color: #52ad9c;
   padding-bottom: 1%;
   border-bottom: 4px solid #47624f;
 `;
-// const NavLinks = styled(Link)`
-//  color: black;
-//  border: 2px solid black;
-//  text-decoration: none;
-//  padding: 0% 2% 0% 2%;
-//  margin-top: 1%;
-//  font-weight: bold;
-//  border-radius: 5px;
-//  background-color: #9ffcdf;
-//  &:hover {
-//    background-color: white;
-//  }
-// `;
+const NavLinks = styled(Link)`
+ color: black;
+ border: 2px solid black;
+ text-decoration: none;
+ padding: 0% 2% 0% 2%;
+ margin-top: 1%;
+ font-weight: bold;
+ border-radius: 5px;
+ background-color: #9ffcdf;
+ &:hover {
+   background-color: white;
+ }
+`;
 const Navo = styled.div`
   background: #9ffcdf;
   borderl: 2px solid black;
@@ -50,23 +40,23 @@ function Nav() {
   return (
     <Navo>
       <NavDiv>
-        <Link to="/register">
-          <NavLinks>Sign-Up</NavLinks>
-        </Link>
-        <Link to="/">
-          <NavLinks>Login</NavLinks>
-        </Link>
-        <Link to="/Chefposts">
-          <NavLinks>Chef Post</NavLinks>
-        </Link>
-        <Link>
-          <NavLinks>Homepage</NavLinks>
-        </Link>
+        <NavLinks to="/register">
+          <p>Sign-Up</p>
+        </NavLinks>
+        <NavLinks to="/">
+          <p>Login</p>
+        </NavLinks>
+        <NavLinks to="/Chefposts">
+          <p>Chef Post</p>
+        </NavLinks>
+        <NavLinks>
+          <p>Homepage</p>
+        </NavLinks>
         <Route exact path="/cheflogin" component={SigninPage} />
         <Route exact path="/chefpostpage" component={ChefPostPage} />
-        <Link to="/">
-          <NavLinks>Log out</NavLinks>
-        </Link>
+        <NavLinks to="/">
+          <p>Log out</p>
+        </NavLinks>
       </NavDiv>
     </Navo>
   );
